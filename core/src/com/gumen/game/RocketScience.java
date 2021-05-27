83c3d82303f667271383a9ac0d53c3bfe9099ad9
HandsomeKoreeZ <alexey-gumen@yandex.ru>
2021-05-27 15:29:20 +0200

    start game project

--- core/src/com/gumen/game/RocketScience.java
+++ core/src/com/gumen/game/RocketScience.java
@@ -3,23 +3,30 @@ package com.gumen.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
+
+import java.util.Vector;
 
 public class RocketScience extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	TextureRegion region;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("HK.png");
+		//region = new TextureRegion(img,50,50,100,100);
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0.27f, 0.709f, 0.737f, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 50, 50);
 		batch.end();
 	}
 	
