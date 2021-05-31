93a2ebaaa24ad85ea298e9c74ca140ce4ccf417f
iAmWhiteBear <78112636+iAmWhiteBear@users.noreply.github.com>
2021-05-31 18:22:40 +0200

    Revert "refactor screen, move picture by touchUp"

--- core/src/com/gumen/game/RocketScience.java
+++ core/src/com/gumen/game/RocketScience.java
@@ -1,17 +1,38 @@
 package com.gumen.game;
 
-import com.badlogic.gdx.Game;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.ScreenUtils;
 
-import screen.MenuScreen;
+import java.util.Vector;
 
-
-public class RocketScience extends Game {
-
-
+public class RocketScience extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	TextureRegion region;
+	
 	@Override
 	public void create () {
-		setScreen(new MenuScreen());
+		batch = new SpriteBatch();
+		img = new Texture("HK.png");
+		//region = new TextureRegion(img,50,50,100,100);
+
 	}
 
-
+	@Override
+	public void render () {
+		ScreenUtils.clear(0.27f, 0.709f, 0.737f, 1);
+		batch.begin();
+		batch.draw(img, 50, 50);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
 }
