1851dc7503995feac24fb9553f29b53ab9809ef3
iAmWhiteBear <78112636+iAmWhiteBear@users.noreply.github.com>
2021-05-31 18:23:01 +0200

    Revert "start game project"

--- core/src/com/gumen/game/RocketScience.java
+++ core/src/com/gumen/game/RocketScience.java
@@ -3,30 +3,23 @@ package com.gumen.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
-
-import java.util.Vector;
 
 public class RocketScience extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	TextureRegion region;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("HK.png");
-		//region = new TextureRegion(img,50,50,100,100);
-
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(0.27f, 0.709f, 0.737f, 1);
+		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 50, 50);
+		batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
